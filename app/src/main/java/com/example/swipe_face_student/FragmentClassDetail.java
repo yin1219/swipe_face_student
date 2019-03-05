708dc8fec9dcf00054830e950f69f056a6c3fe04
yin1219 <kk8992008@gmail.com>
2019-03-05 22:00:54 +0800

    已完成 課程清單40% 20190305-yin

--- app/src/main/java/com/example/swipe_face_student/FragmentClassDetail.java
+++ app/src/main/java/com/example/swipe_face_student/FragmentClassDetail.java
@@ -5,17 +5,24 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.swipe_face_student.Model.Class;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import io.opencensus.tags.Tag;
 
 import static com.example.swipe_face_student.BackHandlerHelper.handleBackPress;
-
 
 
 public class FragmentClassDetail extends Fragment implements FragmentBackHandler {
@@ -23,6 +30,13 @@ public class FragmentClassDetail extends Fragment implements FragmentBackHandler
 
     private String TAG = "ClassDetail";
     private String classId;
+    private Class aclass;
+    private Class firestore_class;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private GridLayout gridLayout;
+    private TextView text_class_id;
+    private TextView text_class_title;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,16 +46,31 @@ public class FragmentClassDetail extends Fragment implements FragmentBackHandler
 
         Bundle args = new Bundle();//fragment傳值
         args = getArguments();//fragment傳值
-        classId=args.getString("info");
-        Log.d(TAG,"classId:" +classId);//fragment傳值
-        Toast.makeText(getContext(),"現在課程代碼是"+classId,Toast.LENGTH_LONG).show();
+        classId = args.getString("info");
+        Log.d(TAG, "classId:" + classId);//fragment傳值
+        Toast.makeText(getContext(), "現在課程代碼是" + classId, Toast.LENGTH_LONG).show();
 
 
         return inflater.inflate(R.layout.fragment_fragment_class_detail, container, false);
     }
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG,"classId2:"+classId);
+        Log.d(TAG, "classId2:" + classId);
+        text_class_title = (TextView) view.findViewById(R.id.text_class_title);
+        text_class_id = (TextView) view.findViewById(R.id.text_class_id);
+        gridLayout = (GridLayout) view.findViewById(R.id.grid_class_detail);
+
+        setClass(new FirebaseCallback(){
+            @Override
+            public void onCallback(Class firestore_class) {
+
+                text_class_title.setText(firestore_class.getClass_name());
+                text_class_id.setText(firestore_class.getClass_id());
+            }
+        });
+
+
+        setSingleEvent(gridLayout);
     }
 
     @Override
@@ -59,7 +88,60 @@ public class FragmentClassDetail extends Fragment implements FragmentBackHandler
         }
     }//fragment 返回鍵
 
+    private void setClass(FirebaseCallback firebaseCallback) {
+        firestore_class = new Class();
+        Log.d(TAG,"setClass class_id:"+classId);
+        Task<DocumentSnapshot> documentSnapshotTask = db.collection("Class")
+                .document(classId)
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        firestore_class = documentSnapshot.toObject(Class.class);
+                        Log.d(TAG, "setClass here");
+                        firebaseCallback.onCallback(firestore_class);
+                    }
+                });
 
 
+    }
+
+    public interface FirebaseCallback {
+        void onCallback(Class firestore_class);
+    }//處理firestore非同步的問題 回調接頭
+
+    // we are setting onClickListener for each element
+    private void setSingleEvent(GridLayout gridLayout) {
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            CardView cardView = (CardView) gridLayout.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "Clicked at index " + finalI,
+                            Toast.LENGTH_SHORT).show();
+                    switch (finalI){
+                        case 0:
+
+                            break;
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+
+                            break;
+                    }
+                }
+            });
+        }
+    }
 
 }

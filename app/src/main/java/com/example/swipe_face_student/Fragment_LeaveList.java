1a07d1ea04cc0f7a66438d46583db31949a0ff76
SuBoLun <suibai123@gmail.com>
2019-05-10 12:39:49 +0800

    5/10 請假介面修改

--- app/src/main/java/com/example/swipe_face_student/Fragment_LeaveList.java
+++ app/src/main/java/com/example/swipe_face_student/Fragment_LeaveList.java
@@ -6,98 +6,101 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.example.swipe_face_student.Adapter.LeaveListAdapter;
-import com.example.swipe_face_student.Model.Leave;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
-public class Fragment_LeaveList extends Fragment implements FragmentBackHandler {
-    private static final String TAG = "LeaveList";
-    private RecyclerView mMainList;
-    private FirebaseFirestore mFirestore;
-    private LeaveListAdapter leaveListAdapter;
-    private String class_id;
-    private String student_id;
+
+public class Fragment_LeaveList extends Fragment {
+    private static final String TAG = "LeaveList001";
+    private FloatingActionButton fab_leave;
     private boolean isAllClass = true;
 
 
-    private List<Leave> leaveList;
+    private String class_id, student_id = "123";
+
 
     OnFragmentSelectedListener mCallback;//Fragment傳值
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Bundle args = getArguments();//fragment傳值
+        student_id = args.getString("student_id");
 
-        Bundle args = new Bundle();//fragment傳值
-        args = getArguments();//fragment傳值
-        Log.d(TAG,"Bundle "+new Bundle());
-//        Log.d(TAG,"args0: "+args.toString());
-        if (args!=null){
-            class_id = args.getString("info");
-            student_id = args.getString("student_id");
-            Log.d(TAG,"args: "+args.toString());
-
-        }else {
-            class_id = null;
-        }
-
+        Log.d(TAG, "TEST arg : " + args.toString());
         Log.d(TAG, "class_id:" + class_id);//fragment傳值
-        Toast.makeText(getContext(), "現在課程:" + class_id, Toast.LENGTH_LONG).show();
+        Log.d(TAG, "student_id:" + student_id);//fragment傳值
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment__leave_list, container, false);
     }
 
-    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        leaveList = new ArrayList<>();
-        leaveListAdapter = new LeaveListAdapter(getActivity().getApplicationContext(), leaveList);
-        FloatingActionButton fab = (FloatingActionButton) getView().findViewById(R.id.fab_leave);
-        mMainList = (RecyclerView) getView().findViewById(R.id.leave_list);
-        mMainList.setHasFixedSize(true);
-        mMainList.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mMainList.setAdapter(leaveListAdapter);
 
-        if (class_id == null) {
-            setAllLeave();
-        }else {
-            setClassLeave();
-        }
+        FragmentTabHost tabHost = (FragmentTabHost) view.findViewById(android.R.id.tabhost);
+        fab_leave = (FloatingActionButton) view.findViewById(R.id.fab_leave);
+        tabHost.setup(getActivity(), getFragmentManager(), R.id.realtabcontent);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //1
+
+        Bundle arg1 = new Bundle();
+        arg1.putString("PassStudent_id", student_id);
+        arg1.putString("PassList_Way", "未審核");
+
+        tabHost.addTab(tabHost.newTabSpec("Checking")
+                        .setIndicator("未審核"),
+                Fragment_LeaveList_View.class,
+                arg1);
+        Log.d(TAG, "TEST LOG RUN Times  " + arg1.toString());
+
+        //2
+
+        Bundle arg2 = new Bundle();
+        arg2.putString("PassStudent_id", student_id);
+        arg2.putString("PassList_Way", "准假");
+        tabHost.addTab(tabHost.newTabSpec("Check0")
+                        .setIndicator("准假"),
+                Fragment_LeaveList_View.class,
+                arg2);
+        //3
+
+        Bundle arg3 = new Bundle();
+        arg3.putString("PassStudent_id", student_id);
+        arg3.putString("PassList_Way", "不准假");
+
+        tabHost.addTab(tabHost.newTabSpec("Check1")
+                        .setIndicator("不准假"),
+                Fragment_LeaveList_View.class,
+                arg3);
+
+        fab_leave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
-                Bundle formLeaveList = new Bundle();
-                formLeaveList.putString("class_id",class_id);
-                if (isAllClass){
-                formLeaveList.putBoolean("isAllClass",true);
-                }else{
-                    formLeaveList.putBoolean("isAllClass",false);
-                }
-                i.putExtras(formLeaveList);
-                i.setClass(getActivity().getApplicationContext(), LeaveApplications.class);
-                startActivity(i);
+                    Intent i = new Intent();
+                    Bundle formLeaveList = new Bundle();
+                    formLeaveList.putString("class_id",class_id);
+                    if (isAllClass){
+                        formLeaveList.putBoolean("isAllClass",true);
+                    }else{
+                        formLeaveList.putBoolean("isAllClass",false);
+                    }
+                    i.putExtras(formLeaveList);
+                    i.setClass(getActivity().getApplicationContext(), LeaveApplications.class);
+                    startActivity(i);
             }
         });
-
-
     }
+
+
 
     @Override
     public void onAttach(Context context) {
@@ -107,77 +110,6 @@ public class Fragment_LeaveList extends Fragment implements FragmentBackHandler 
         } catch (ClassCastException e) {
             throw new ClassCastException(context.toString() + "Mush implement OnFragmentSelectedListener ");
         }
-    }
-
-
-    @Override
-    public boolean onBackPressed() {
-        return BackHandlerHelper.handleBackPress(this);
-    }//fragment 返回鍵
-
-
-    private void setAllLeave() {
-        isAllClass=true;
-        mFirestore = FirebaseFirestore.getInstance();
-
-        mFirestore.collection("Leave").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
-
-                if (e != null) {
-                    Log.d(TAG, "error" + e.getMessage());
-                }
-
-                for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
-
-                    if (doc.getType() == DocumentChange.Type.ADDED) {
-
-                        String leaveId = doc.getDocument().getId();
-
-                        Leave leave = doc.getDocument().toObject(Leave.class).withId(leaveId);
-                        leaveList.add(leave);
-
-                        leaveListAdapter.notifyDataSetChanged();
-                    }
-
-                }
-            }
-
-
-        });
-    }
-
-
-    private void setClassLeave() {
-        isAllClass=false;
-
-        mFirestore = FirebaseFirestore.getInstance();
-
-        mFirestore.collection("Leave").whereEqualTo("class_id", class_id).addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
-
-                if (e != null) {
-                    Log.d(TAG, "error" + e.getMessage());
-                }
-
-                for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
-
-                    if (doc.getType() == DocumentChange.Type.ADDED) {
-
-                        String leaveId = doc.getDocument().getId();
-
-                        Leave leave = doc.getDocument().toObject(Leave.class).withId(leaveId);
-                        leaveList.add(leave);
-
-                        leaveListAdapter.notifyDataSetChanged();
-                    }
-
-                }
-            }
-
-
-        });
     }
 
 }
